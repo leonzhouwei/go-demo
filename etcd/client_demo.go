34d82696a4c1efaaf539bb6972e1fedf1a802ac1
Wei Zhou <zhouwei@qiniu.com>
2016-01-11 15:50:21 +0800

    style: format all

--- etcd/client_demo.go
+++ etcd/client_demo.go
@@ -11,8 +11,8 @@ import (
 
 func main() {
 	cfg := client.Config{
-		Endpoints:               []string{"http://127.0.0.1:2379"},
-		Transport:               client.DefaultTransport,
+		Endpoints: []string{"http://127.0.0.1:2379"},
+		Transport: client.DefaultTransport,
 		// set timeout per request to fail fast when the target endpoint is unavailable
 		HeaderTimeoutPerRequest: time.Second,
 	}
--- go/syscall/disk_info.go
+++ go/syscall/disk_info.go
@@ -7,7 +7,7 @@ import (
 )
 
 type DiskStatus struct {
-	All uint64
+	All  uint64
 	Used uint64
 	Free uint64
 }
@@ -18,12 +18,12 @@ func main() {
 		log.Fatal(err)
 	} else {
 		fmt.Println(ds)
-		fmt.Println(" all:", ds.All / 1024 / 1024 / 1024, "GB")
+		fmt.Println(" all:", ds.All/1024/1024/1024, "GB")
 		var usedfloat64 float64 = float64(ds.Used)
 		var allfloat64 float64 = float64(ds.All)
 		usedRatio := usedfloat64 / allfloat64
-		fmt.Printf("used: %.2f%%, %v GB\n", usedRatio * 100.0, ds.Used / 1024 / 1024 / 1024)
-		fmt.Printf("free: %.2f%%, %v GB\n", (1.0 - usedRatio) * 100.0, ds.Free / 1024 / 1024 / 1024)
+		fmt.Printf("used: %.2f%%, %v GB\n", usedRatio*100.0, ds.Used/1024/1024/1024)
+		fmt.Printf("free: %.2f%%, %v GB\n", (1.0-usedRatio)*100.0, ds.Free/1024/1024/1024)
 	}
 }
 
