47a6fde5c8df4b9aeca3ca57ca3511a227e691be
Wei Zhou <zhouwei@qiniu.com>
2016-01-11 17:36:40 +0800

    fix: make golint happy

--- go/syscall/disk_info.go
+++ go/syscall/disk_info.go
@@ -6,28 +6,29 @@ import (
 	"syscall"
 )
 
+// DiskStatus disk capacity status
 type DiskStatus struct {
-	All  uint64
-	Used uint64
-	Free uint64
+	All  uint64 // count of all btyes of a disk
+	Used uint64 // used of used btyes of a disk
+	Free uint64 // free of free btyes of a disk
 }
 
 func main() {
-	ds, err := DiskUsageUnixLike(".")
+	ds, err := diskUsageUnixLike(".")
 	if err != nil {
 		log.Fatal(err)
 	} else {
 		fmt.Println(ds)
 		fmt.Println(" all:", ds.All/1024/1024/1024, "GB")
-		var usedfloat64 float64 = float64(ds.Used)
-		var allfloat64 float64 = float64(ds.All)
+		usedfloat64 := float64(ds.Used)
+		allfloat64 := float64(ds.All)
 		usedRatio := usedfloat64 / allfloat64
 		fmt.Printf("used: %.2f%%, %v GB\n", usedRatio*100.0, ds.Used/1024/1024/1024)
 		fmt.Printf("free: %.2f%%, %v GB\n", (1.0-usedRatio)*100.0, ds.Free/1024/1024/1024)
 	}
 }
 
-func DiskUsageUnixLike(path string) (*DiskStatus, error) {
+func diskUsageUnixLike(path string) (*DiskStatus, error) {
 	fs := syscall.Statfs_t{}
 	err := syscall.Statfs(path, &fs)
 	if err != nil {
